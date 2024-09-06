92762d375115c0972dc7cfbd6370565955c83bf8
karinatabares1 <marianaocampomedina@gmail.com>
2024-09-05 21:19:37 -0500

    ArrayList

--- ArrayList/Ejercicio2.java
+++ ArrayList/Ejercicio2.java
@@ -1,10 +1,20 @@
 package ArrayList;
-
+import java.util.ArrayList;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  ArrayList<Integer> numbers = new ArrayList<>();
 
+	        // Agregar elementos al ArrayList
+	        numbers.add(10);
+	        numbers.add(20);
+	        numbers.add(30);
+
+	        // Mostrar elementos del ArrayList
+	        System.out.println("Elementos del ArrayList:");
+	        for (int number : numbers) {
+	            System.out.println(number);
+
+	        }
 	}
-
 }
--- ArrayList/Ejercicio3.java
+++ ArrayList/Ejercicio3.java
@@ -1,9 +1,19 @@
 package ArrayList;
-
+import java.util.ArrayList;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		        ArrayList<Integer> numbers = new ArrayList<>();
+		        numbers.add(5);
+		        numbers.add(15);
+		        numbers.add(25);
+
+		        int sum = 0;
+		        for (int number : numbers) {
+		            sum += number;
+		        }
+
+		        System.out.println("Suma de los elementos: " + sum);
 
 	}
 
